370240fce1a1445d8f9f0231b9936f440f072317
James Jervis <jjervis@relivinc.com>
2016-10-19 18:28:15 -0500

    Update

--- src/Exception/ErrorException.php
+++ src/Exception/ErrorException.php
@@ -3,7 +3,7 @@
 namespace RcmErrorHandler2\Exception;
 
 /**
- * Class ErrorException
+ * Class ErrorException - Wrapper for Errors and Exceptions
  *
  * @author    James Jervis <jjervis@relivinc.com>
  * @copyright 2016 Reliv International
@@ -13,6 +13,11 @@ namespace RcmErrorHandler2\Exception;
 class ErrorException extends \ErrorException
 {
     /**
+     * @var \Throwable|null
+     */
+    protected $actualError;
+
+    /**
      * @var
      */
     protected $context;
@@ -20,18 +25,53 @@ class ErrorException extends \ErrorException
     /**
      * ErrorException constructor.
      *
-     * @param string     $message
-     * @param int        $code
-     * @param int        $severity
-     * @param string     $filename
-     * @param int        $lineno
+     * @param string $message
+     * @param int $code
+     * @param int $severity
+     * @param string $filename
+     * @param int $lineno
      * @param \Exception $previous
-     * @param array      $context
+     * @param \Exception $previous
+     * @param array $context
      */
-    public function __construct($message, $code, $severity, $filename, $lineno, $previous = null, $context = [])
-    {
+    public function __construct(
+        $message,
+        $code,
+        $severity,
+        $filename,
+        $lineno,
+        $previous = null,
+        $actualError = null,
+        $context = []
+    ) {
+        $this->actualError = $actualError;
         $this->context = $context;
         parent::__construct($message, $code, $severity, $filename, $lineno, $previous);
+    }
+
+    /**
+     * getActualError
+     *
+     * @return \Throwable|null
+     */
+    public function getActualException()
+    {
+        return $this->actualError;
+    }
+
+    /**
+     * throwActual
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function throwActual()
+    {
+        if ($this->actualError instanceof \Throwable) {
+            throw $this->actualError;
+        }
+
+        throw $this;
     }
 
     /**
@@ -39,7 +79,8 @@ class ErrorException extends \ErrorException
      *
      * @return array
      */
-    public function getContext() {
+    public function getContext()
+    {
         return $this->context;
     }
 }
