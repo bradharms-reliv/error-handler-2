2699cf4aaec7cdcfb49abc154ccf152df1ccae56
James Jervis <jjervis@relivinc.com>
2016-12-09 16:25:53 -0600

    Dont create JS is it is disabled

--- src/Middleware/ClientErrorLoggerJsController.php
+++ src/Middleware/ClientErrorLoggerJsController.php
@@ -2,6 +2,7 @@
 
 namespace RcmErrorHandler2\Middleware;
 
+use RcmErrorHandler2\Config\JsLogConfig;
 use Zend\Stratigility\Http\Request;
 use Zend\Stratigility\Http\Response;
 
@@ -16,17 +17,58 @@ use Zend\Stratigility\Http\Response;
 class ClientErrorLoggerJsController
 {
     /**
+     * @var JsLogConfig
+     */
+    protected $jsLogConfig;
+
+    /**
+     * ClientErrorLoggerJsController constructor.
+     *
+     * @param JsLogConfig $jsLogConfig
+     */
+    public function __construct(
+        JsLogConfig $jsLogConfig
+    ) {
+        $this->jsLogConfig = $jsLogConfig;
+    }
+
+    /**
+     * getOption
+     *
+     * @param string $key
+     * @param null   $default
+     *
+     * @return null
+     */
+    protected function getOption($key, $default = null)
+    {
+        $options = $this->jsLogConfig->get('options', []);
+
+        if (array_key_exists($key, $options)) {
+            return $options[$key];
+        }
+
+        return $default;
+    }
+
+    /**
      * __invoke
      *
      * @param Request       $request
      * @param Response      $response
      * @param callable|null $next
      *
-     * @return $response
+     * @return Response $response
      */
     public function __invoke(Request $request, Response $response, callable $next = null)
     {
-        $js = file_get_contents(__DIR__ . '/../../public/js-error-logger.js');
+        $hasJsLogging = $this->getOption('logJsErrors', false);
+
+        $js = '/* JavaScript logging not enabled */';
+
+        if ($hasJsLogging) {
+            $js = file_get_contents(__DIR__ . '/../../public/js-error-logger.js');
+        }
 
         $body = $response->getBody();
 
